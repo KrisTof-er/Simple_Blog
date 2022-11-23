8261f41cf3d113970801fa1689f83b89a0914200
kristof <sichkardenis5@gmail.com>
2022-11-23 21:11:51 +0200

    :baseball: Add base.html file and articles page

--- blog/api.py
+++ blog/api.py
@@ -6,3 +6,8 @@ blog_router = Blueprint('blog', __name__)
 @blog_router.route('/')
 def index():
     return render_template('index.html')
+
+
+@blog_router.route('/articles')
+def get_articles():
+    return render_template('articles.html')
