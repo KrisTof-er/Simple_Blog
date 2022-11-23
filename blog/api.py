bbec0da84d68318b849745c0b6ac6a4c4298ab54
kristof <sichkardenis5@gmail.com>
2022-11-23 22:12:55 +0200

    :page_facing_up: Add Single article page

--- blog/api.py
+++ blog/api.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template
 
+from blog.db import ArticleDB
+
 blog_router = Blueprint('blog', __name__)
+db = ArticleDB()
 
 
 @blog_router.route('/')
@@ -10,4 +13,11 @@ def index():
 
 @blog_router.route('/articles')
 def get_articles():
-    return render_template('articles.html')
+    articles = db.get_all()
+    return render_template('articles.html', articles=articles)
+
+
+@blog_router.route('/article/<int:id_>')
+def get_article(id_):
+    article = db.get_one(id_=id_)
+    return render_template('article.html', article=article)
--- blog/db.py
+++ blog/db.py
@@ -1,13 +1,32 @@
-class PostDB:
+class ArticleDB:
     storage = [
         {
             "id": 1,
             "title": "title1",
             "text": """
-            textTEXTtext textTEXTtext,textTEXTtext. textTEXTtext-textTEXTtext
-            """
+                textTEXTtext textTEXTtext,textTEXTtext. textTEXTtext-textTEXTtext
+                """
+        },
+        {
+            "id": 2,
+            "title": "title2",
+            "text": """
+                textTEXTtext textTEXTtext,textTEXTtext. textTEXTtext-textTEXTtext
+                """
+        },
+        {
+            "id": 3,
+            "title": "title3",
+            "text": """
+                textTEXTtext textTEXTtext,textTEXTtext. textTEXTtext-textTEXTtext
+                """
         }
     ]
 
     def get_all(self):
         return self.storage
+
+    def get_one(self, id_):
+        for article in self.storage:
+            if article['id'] == id_:
+                return article
